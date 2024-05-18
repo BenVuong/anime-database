43225bd4c6f8cb407b16c74e7b47768e65c793fe
Ben G Vuong <ben.vuong2000@gmail.com>
2024-05-18 11:42:48 -0500

    top anime shown in gird of cards with title and cover art

--- src/component/homepage.jsx
+++ src/component/homepage.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-
+import { Grid, Card, InputLabel, TextField, CardContent } from "@mui/material";
 function Homepage() {
   const [animeData, setAnimeData] = useState([]);
-  const [title, setTitle] = useState("");
+
   const API_URL = "https://api.jikan.moe/v4";
 
   const getTopAnime = async () => {
-    const response = await fetch(`${API_URL}/top/anime`);
+    const response = await fetch(`${API_URL}/top/anime?page=2`);
     setAnimeData(await response.json());
     console.log(animeData);
   };
@@ -18,15 +18,23 @@ function Homepage() {
   return (
     <div>
       <button onClick={getTopAnime}>top anime</button>
-      {title}
 
-      {animeData.data?.map((anime, index) => {
-        return (
-          <tr key={index}>
-            <td>{anime.title_english}</td>
-          </tr>
-        );
-      })}
+      <Grid container spacing={4}>
+        {animeData.data?.map((anime, index) => {
+          return (
+            <Grid item xs={4}>
+              <Card>
+                <CardContent>
+                  <Grid item>{anime.title_english}</Grid>
+                  <Grid item>
+                    <img src={anime.images.jpg.image_url}></img>
+                  </Grid>
+                </CardContent>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
