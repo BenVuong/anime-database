3621b7bea1824c738c20fa8759d2c6e8963df369
Ben G Vuong <ben.vuong2000@gmail.com>
2024-05-19 13:22:58 -0500

    added search function

--- src/component/homepage.jsx
+++ src/component/homepage.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Grid, Card, InputLabel, TextField, CardContent } from "@mui/material";
+import { useParams } from "react-router-dom";
 function Homepage() {
   const [animeData, setAnimeData] = useState([]);
-
+  const [pageNum, setPageNum] = useState(1);
+  const [searchAnimeName, setSearchAnimeName] = useState("");
   const API_URL = "https://api.jikan.moe/v4";
 
   const getTopAnime = async () => {
-    const response = await fetch(`${API_URL}/top/anime?page=2`);
+    const response = await fetch(`${API_URL}/top/anime?`);
+    setAnimeData(await response.json());
+    console.log(animeData);
+  };
+  const getPopularAnime = async () => {
+    const response = await fetch(`${API_URL}/top/anime?filter=bypopularity`);
     setAnimeData(await response.json());
     console.log(animeData);
   };
@@ -15,17 +22,49 @@ function Homepage() {
     getTopAnime();
   }, []);
 
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    if (searchAnimeName === "") {
+    } else {
+      searchAnime(searchAnimeName);
+    }
+  };
+
+  async function searchAnime(search) {
+    const response = await fetch(`${API_URL}/anime?q=${search}`);
+    const data = await response.json();
+    setAnimeData(data);
+    console.log(animeData);
+  }
+
   return (
     <div>
-      <button onClick={getTopAnime}>top anime</button>
-
       <Grid container spacing={4}>
-        {animeData.data?.map((anime, index) => {
+        <Grid item xs={12}>
+          <form onSubmit={handleSearchSubmit}>
+            <div className="mb-2">
+              <TextField
+                id="outlined-basic"
+                label="Enter in title of an anime"
+                onChange={(e) => setSearchAnimeName(e.target.value)}
+                helperText="Once entered its info will be automatically filled out"
+              ></TextField>
+            </div>
+            <button className="btn btn-success"> Search Anime</button>
+          </form>
+          <div>page: {pageNum}</div>
+          <button onClick={getPopularAnime}>Popular anime</button>
+        </Grid>
+        {animeData.data?.map((anime) => {
           return (
             <Grid item xs={4}>
               <Card>
                 <CardContent>
-                  <Grid item>{anime.title_english}</Grid>
+                  <Grid item>
+                    <h2>{anime.title_english}</h2>
+                  </Grid>
+                  <Grid item>{anime.title}</Grid>
+                  <Grid item>Score: {anime.score}</Grid>
                   <Grid item>
                     <img src={anime.images.jpg.image_url}></img>
                   </Grid>
