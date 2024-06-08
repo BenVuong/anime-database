96ae7682cab49b204232e7a4d5de514c3b978d04
Ben G Vuong <ben.vuong2000@gmail.com>
2024-06-08 10:32:42 -0500

    formatted character page and added basic character bio

--- src/component/characters.jsx
+++ src/component/characters.jsx
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { Title, Pillar, Item } from "./style";
 import "./../App.css";
+import {
+  Box,
+  Grid,
+  Divider,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Card,
+  CardContent,
+  CardHeader,
+} from "@mui/material";
 function Characters() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const API_URL = "https://api.jikan.moe/v4";
   const [characterInfo, setCharacterInfo] = useState([]);
+  const [characterImage, setCharacterImage] = useState("");
   async function getCharacters(id) {
     const response = await fetch(`${API_URL}/characters/${id}/full`);
     const data = await response.json();
     setCharacterInfo(data.data);
+    setCharacterImage(data.data.images.jpg.image_url);
+    console.log(characterInfo);
   }
 
   useEffect(() => {
@@ -16,10 +32,38 @@ function Characters() {
     console.log(characterInfo);
   }, []);
   return (
-    <div>
-      {characterInfo.name}
-      <div className="card-text">{characterInfo.about}</div>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        <Grid sm={12}>
+          <Title>{characterInfo.name}</Title>
+        </Grid>
+        <Grid xs={15} sm={2}>
+          <Pillar>
+            <Item>
+              <img src={characterImage} style={{ maxWidth: "100%" }}></img>
+            </Item>
+            <Item>
+              Animeography:
+              <Divider />
+              <Item></Item>
+            </Item>
+            <Item>
+              Mangaography:
+              <Divider />
+              <Item></Item>
+            </Item>
+            <Item>
+              <button className="btn btn-info" onClick={() => navigate(-1)}>
+                Back
+              </button>
+            </Item>
+          </Pillar>
+        </Grid>
+        <Grid xs={12} sm={10}>
+          <Item className="card-text"> {characterInfo.about}</Item>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
